c406bf83ee08b03ec5e7ab12e53d8ba8215d4182
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-04-29 20:07:54 +0300

    path change

--- agenttools.py
+++ agenttools.py
@@ -10,12 +10,12 @@ class AgentTools(QueryDataBase):
     def __init__(self):
         super().__init__()
         current_dir = os.path.dirname(__file__)
-        self.CHROMA_PATH = os.path.join(current_dir, './data/', 'CHROMA_PATH0(local)')
-        self.CHROMA_PATH1 = os.path.join(current_dir, './data/', 'CHROMA_PATH0(local)') #fix this
-        self.CHROMA_PATH2 = os.path.join(current_dir, './data/', 'CHROMA_PATH2(personal)')
-        self.CHROMA_PATH3 = os.path.join(current_dir,'./data/','CHROMA_PATH3(books)')
-        self.CHROMA_PATH4 = os.path.join(current_dir, './data/', 'CHROMA_PATH4(research_papers)')
-        self.CHROMA_PATH5 = os.path.join(current_dir,'./data/','CHROMA_PATH5(assistant_info)')
+        self.CHROMA_PATH = os.path.join(current_dir, './data/CHROMA_PATH0(local)')
+        self.CHROMA_PATH1 = os.path.join(current_dir, './data/CHROMA_PATH0(local)') #fix this
+        self.CHROMA_PATH2 = os.path.join(current_dir, './data/CHROMA_PATH2(personal)')
+        self.CHROMA_PATH3 = os.path.join(current_dir,'./data/CHROMA_PATH3(books)')
+        self.CHROMA_PATH4 = os.path.join(current_dir, './data/CHROMA_PATH4(research_papers)')
+        self.CHROMA_PATH5 = os.path.join(current_dir,'./data/CHROMA_PATH5(assistant_info)',)
 
     
     
